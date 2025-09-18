986ffc6fad9756c8f57151bffa2f6a00f5d9cf20
maheen2601 <maheenkhan@immcorporate.com>
2025-09-18 16:59:12 +0500

    Update admin.py

--- backend/attendance/admin.py
+++ backend/attendance/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Employee
 from .models import Employee, Attendance
 # Register your models here.
+from .models import PolicySettings
 
 
 @admin.register(Employee)
@@ -12,3 +13,8 @@ class EmployeeAdmin(admin.ModelAdmin):
 class AttendanceAdmin(admin.ModelAdmin):
     list_display = ('employee', 'date', 'status', 'mode')
     list_filter = ('status', 'mode', 'date')
+
+@admin.register(PolicySettings)
+class PolicySettingsAdmin(admin.ModelAdmin):
+    list_display = ("grace_minutes","late_notice_minutes","min_daily_hours")
+
